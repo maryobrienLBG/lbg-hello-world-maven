d019c1ff539aca8145406769e1e217b041e1cd0b
MaryOBrien <you@example.com>
2024-11-04 14:20:10 +0000

    new add

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("Mary"));
+        System.out.println(sayHelloToSomeone("Mary!"));
         System.out.println(sayGoodbye());
     }
 
